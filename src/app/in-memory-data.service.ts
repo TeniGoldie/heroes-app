860518b8a72e4122a8d7f5b06b5e105cf101a591
TeniGoldie <ta.zharska@gmail.com>
2017-07-04 17:10:03 +0300

    heroes names edit

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,17 +2,17 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const heroes = [
-            { id: 0,  name: 'Zero' },
-            { id: 11, name: 'Mr. Nice' },
-            { id: 12, name: 'Narco' },
-            { id: 13, name: 'Bombasto' },
-            { id: 14, name: 'Celeritas' },
-            { id: 15, name: 'Magneta' },
-            { id: 16, name: 'RubberMan' },
-            { id: 17, name: 'Dynama' },
-            { id: 18, name: 'Dr IQ' },
-            { id: 19, name: 'Magma' },
-            { id: 20, name: 'Tornado' }
+            { id: 0,  name: 'Batman' },
+            { id: 1, name: 'Superman' },
+            { id: 2, name: 'WonderWoman' },
+            { id: 3, name: 'Aquaman' },
+            { id: 4, name: 'Ironman' },
+            { id: 5, name: 'Spiderman' },
+            { id: 6, name: 'Flash' },
+            { id: 7, name: 'GreenArrow' },
+            { id: 8, name: 'Shazam' },
+            { id: 9, name: 'Atom' },
+            { id: 10, name: 'GreenLantern' }
         ];
         return {heroes};
     }
